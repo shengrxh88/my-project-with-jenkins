d48ffb0e95a2ecdec5b4d01ddc36675349b95ee1
李春海 <lichunhai@i-i.ai>
2018-07-11 20:01:18 +0800

    init

---
+++ src/main/java/shizaizhineng/my_application_on_mac/App.java
@@ -0,0 +1,13 @@
+package shizaizhineng.my_application_on_mac;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
